f4e30be7fb8a27d06ede56c748e93cd4a414dadb
CrizUP <Cris_up@outlook.com>
2020-08-06 02:16:01 -0500

    Card de auto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './components/demo.component';
+import { AutoCardComponent } from './components/auto-card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    AutoCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/auto-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-auto-card',
+    templateUrl: './auto-card.component.html',
+    styleUrls: ['./auto-card.component.scss'],
+})
+
+export class AutoCardComponent  { }
