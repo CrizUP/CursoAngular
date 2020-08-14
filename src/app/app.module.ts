c89cf88a6eccc4781abbda8f75a48dc4c27c4335
CrizUP <Cris_up@outlook.com>
2020-08-14 00:32:58 -0500

    Uso de estilos para los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './components/demo.component';
+import { DemoComponent } from './components/demo/demo.component';
 import { AutoCardComponent } from './components/auto-card.component';
+import { AutoComponent } from './components/auto/auto.component';
+import { AutosComponent } from './components/autos/autos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    AutoCardComponent
+    AutoCardComponent,
+    AutoComponent,
+    AutosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/autos/autos.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Auto } from '../../auto.model';
+
+@Component({
+  selector: 'app-autos',
+  templateUrl: './autos.component.html',
+  styleUrls: ['./autos.component.scss']
+})
+export class AutosComponent implements OnInit, OnChanges {
+  autos: Auto[] = [
+    {
+      año: 2020,
+      descripcion: 'Es un auto veloz',
+      id: '1',
+      modelo: 'Modelo Advance',
+      nombre: 'BMW',
+      imagen: 'https://cnet4.cbsistatic.com/img/HLLu44FPHv-LnJ0dxJDvbsy-Bh8=/868x488/2019/09/24/21e34422-2496-4472-bb63-12381d85455f/00000img-00000-burst20190624080825073-cover.jpg'
+      // imagen: '../../../assets/images/i'
+    },
+    {
+      año: 2020,
+      descripcion: 'Es un auto veloz',
+      id: '2',
+      modelo: 'Modelo Advance',
+      nombre: 'Bentli',
+      imagen: 'https://cnet4.cbsistatic.com/img/HLLu44FPHv-LnJ0dxJDvbsy-Bh8=/868x488/2019/09/24/21e34422-2496-4472-bb63-12381d85455f/00000img-00000-burst20190624080825073-cover.jpg'
+    },
+    {
+      año: 2020,
+      descripcion: 'Es un auto veloz',
+      id: '2',
+      modelo: 'Modelo Advance',
+      nombre: 'Bentli',
+      imagen: 'https://cnet4.cbsistatic.com/img/HLLu44FPHv-LnJ0dxJDvbsy-Bh8=/868x488/2019/09/24/21e34422-2496-4472-bb63-12381d85455f/00000img-00000-burst20190624080825073-cover.jpg'
+    }
+  ];
+  contador = 0;
+  @Input() title: string;
+  constructor() {
+    console.log('1.constructor');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('2.onchanges');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('3.oninit');
+  }
+  addCarrosVendidos(): void{
+    this.contador++;
+  }
+
+}
